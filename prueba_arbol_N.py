a7d533f033da7aa7801da7b0621f5d0c6932e354
omarpach <omy250@gmail.com>
2025-02-10 23:27:03 -0700

    Fix: Error en funcion descarga_archivo y descomprimir

--- prueba_arbol_N.py
+++ prueba_arbol_N.py
@@ -2,41 +2,38 @@ import utileria as ut
 import arboles_numericos as an
 import os
 import random
+import math
 
 # Descarga y descomprime los datos
 
 url = "https://archive.ics.uci.edu/static/public/17/breast+cancer+wisconsin+diagnostic.zip"
 archivo = "datos/cancer.zip"
 archivo_datos = "datos/wdbc.data"
-atributos = ['ID', 'Diagnosis'] + [f'feature_{i}' for i in range(1, 31)]
+atributos = ["ID", "Diagnosis"] + [f"feature_{i}" for i in range(1, 31)]
 
 # Descarga datos
-if not os.path.exists("datos"):
-    os.makedirs("datos")
-if not os.path.exists(archivo):
-    ut.descarga_datos(url, archivo)
-    ut.descomprime_zip(archivo)
+# if not os.path.exists("datos"):
+#     os.makedirs("datos")
+# if not os.path.exists(archivo):
+#     ut.descarga_datos(url, archivo)
+#     ut.descomprime_zip(archivo)
 
-#Extrae datos y convierte a numericos
-datos = ut.lee_csv(
-    archivo_datos,
-    atributos=atributos,
-    separador=","
-)
+# Extrae datos y convierte a numericos
+datos = ut.lee_csv(archivo_datos, atributos=atributos, separador=",")
 for d in datos:
-    d['Diagnosis'] = 1 if d['Diagnosis'] == 'M' else 0
+    d["Diagnosis"] = 1 if d["Diagnosis"] == "M" else 0
     for i in range(1, 31):
-        d[f'feature_{i}'] = float(d[f'feature_{i}'])
-    del(d['ID'])
+        d[f"feature_{i}"] = float(d[f"feature_{i}"])
+    del d["ID"]
 
 # Selecciona los artributos
-target = 'Diagnosis'
-atributos = [target] + [f'feature_{i}' for i in range(1, 31)]
+target = "Diagnosis"
+atributos = [target] + [f"feature_{i}" for i in range(1, 31)]
 
 # Selecciona un conjunto de entrenamiento y de validación
 random.seed(42)
 random.shuffle(datos)
-N = int(0.8*len(datos))
+N = int(0.8 * len(datos))
 datos_entrenamiento = datos[:N]
 datos_validacion = datos[N:]
 
@@ -44,30 +41,31 @@ datos_validacion = datos[N:]
 errores = []
 for profundidad in [1, 3, 5, 10, 15, 20, 30]:
     arbol = an.entrena_arbol(
-        datos_entrenamiento, 
-        target, 
-        atributos, 
-        max_profundidad=profundidad
+        datos_entrenamiento,
+        target,
+        atributos,
+        max_profundidad=profundidad,
+        acc_nodo=1,
+        min_ejemplos=0,
+        variables_seleccionadas=math.trunc(math.sqrt(len(atributos))),
     )
     error_en_muestra = an.evalua_arbol(arbol, datos_entrenamiento, target)
     error_en_validacion = an.evalua_arbol(arbol, datos_validacion, target)
-    errores.append( (profundidad, error_en_muestra, error_en_validacion) )
-    
+    errores.append((profundidad, error_en_muestra, error_en_validacion))
+
 # Muestra los errores
-print('d'.center(10) + 'Ein'.center(15) + 'E_out'.center(15))
-print('-' * 40)
+print("d".center(10) + "Ein".center(15) + "E_out".center(15))
+print("-" * 40)
 for profundidad, error_entrenamiento, error_validacion in errores:
     print(
-        f'{profundidad}'.center(10) 
-        + f'{error_entrenamiento:.2f}'.center(15) 
-        + f'{error_validacion:.2f}'.center(15)
+        f"{profundidad}".center(10)
+        + f"{error_entrenamiento:.2f}".center(15)
+        + f"{error_validacion:.2f}".center(15)
     )
-print('-' * 40 + '\n')
+print("-" * 40 + "\n")
 
-#Entrena con la mejor profundidad
+# Entrena con la mejor profundidad
 arbol = an.entrena_arbol(datos, target, atributos, max_profundidad=3)
 error = an.evalua_arbol(arbol, datos_entrenamiento, target)
-print(f'Error del modelo seleccionado entrenado con TODOS los datos: {error:.2f}')
+print(f"Error del modelo seleccionado entrenado con TODOS los datos: {error:.2f}")
 an.imprime_arbol(arbol)
-
-    
--- prueba_arbol_Q.py
+++ prueba_arbol_Q.py
@@ -5,11 +5,11 @@ import random
 
 # Descarga y descomprime los datos
 
-url = 'https://archive.ics.uci.edu/static/public/19/car+evaluation.zip'
-archivo = 'datos/car.zip'
-archivo_datos = 'datos/car.data'
-atributos = ['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety', 'class']
-target = 'class'
+url = "https://archive.ics.uci.edu/static/public/19/car+evaluation.zip"
+archivo = "datos/car.zip"
+archivo_datos = "datos/car.data"
+atributos = ["buying", "maint", "doors", "persons", "lug_boot", "safety", "class"]
+target = "class"
 
 # url = 'https://archive.ics.uci.edu/static/public/73/mushroom.zip'
 # archivo = 'datos/mushroom.zip'
@@ -17,20 +17,20 @@ target = 'class'
 # atributos = ['class', 'cap-shape', 'cap-surface', 'cap-color', 'bruises', 'odor', 'gill-attachment', 'gill-spacing', 'gill-size', 'gill-color', 'stalk-shape', 'stalk-root', 'stalk-surface-above-ring', 'stalk-surface-below-ring', 'stalk-color-above-ring', 'stalk-color-below-ring', 'veil-type', 'veil-color', 'ring-number', 'ring-type', 'spore-print-color', 'population', 'habitat']
 # target = 'class'
 
-if not os.path.exists('datos'):
-    os.makedirs('datos')
-if not os.path.exists(archivo):
-    ut.descarga_datos(url, archivo)
-    ut.descomprime_zip(archivo)
-    
+# if not os.path.exists("datos"):
+#     os.makedirs("datos")
+# if not os.path.exists(archivo):
+#     ut.descarga_datos(url, archivo)
+#     ut.descomprime_zip(archivo)
+
 # Lee los datos
-datos = ut.lee_csv(archivo_datos, atributos=atributos, separador=',')
+datos = ut.lee_csv(archivo_datos, atributos=atributos, separador=",")
 
 
 # Selecciona un conjunto de entrenamiento y de validación
 random.seed(42)
 random.shuffle(datos)
-N = int(0.8*len(datos))
+N = int(0.8 * len(datos))
 datos_entrenamiento = datos[:N]
 datos_validacion = datos[N:]
 
@@ -38,25 +38,22 @@ datos_validacion = datos[N:]
 errores = []
 for profundidad in [1, 3, 5, None]:
     arbol = ac.entrena_arbol(
-        datos_entrenamiento, 
-        target, 
-        atributos, 
-        max_profundidad=profundidad
+        datos_entrenamiento, target, atributos, max_profundidad=profundidad
     )
     error_en_muesta = ac.evalua_arbol(arbol, datos_entrenamiento, target)
     error_en_validacion = ac.evalua_arbol(arbol, datos_validacion, target)
-    errores.append( (profundidad, error_en_muesta, error_en_validacion) )
+    errores.append((profundidad, error_en_muesta, error_en_validacion))
 
 # Muetsra los errores
-print('d'.center(10) + 'Ein'.center(15) + 'E_out'.center(15))
-print('-' * 40)
+print("d".center(10) + "Ein".center(15) + "E_out".center(15))
+print("-" * 40)
 for profundidad, error_entrenamiento, error_validacion in errores:
     print(
-        f'{profundidad}'.center(10) 
-        + f'{error_entrenamiento:.4f}'.center(15) 
-        + f'{error_validacion:.4f}'.center(15)
+        f"{profundidad}".center(10)
+        + f"{error_entrenamiento:.4f}".center(15)
+        + f"{error_validacion:.4f}".center(15)
     )
-print('-' * 40 + '\n')    
+print("-" * 40 + "\n")
 
 
 # Y Ahora entrenamos un árbol por única vez con todos los datos
